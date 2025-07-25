70ab1df3ee0fad4607fc8ec5e3936d49d143de45
Jorge_Villa <jorgevihe@gmail.com>
2025-07-25 17:53:17 -0600

    Adding documents

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+
+const text = ref('');
+
+</script>
+
+<template>
+<div>
+
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
