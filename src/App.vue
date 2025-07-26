3a8a56c3515c3c898c80f081fed023338142cf42
Jorge_Villa <jorgevihe@gmail.com>
2025-07-26 13:23:08 -0600

    Dark/Light button added as 'theme', dark mode configuration in main.js, preset configuration and theme configured

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <script setup> 
-import { ref } from 'vue'; // Importing Vue's ref for reactivity
-import InputText from 'primevue/inputtext';
 
-const text = ref('')
-{{'text'}}; // Reactive variable to hold input text
+import { ref } from 'vue'; // Importing Vue's ref for reactivity
+import Button from 'primevue/button'; // Importing PrimeVue Button component
+
+function toggleDarkMode() {
+    document.documentElement.classList.toggle('my-app-dark');
+}
 
 </script>
 
 <template>
 
+<Button label="Theme" @click="toggleDarkMode()" />
+
 <div>
-<h1 class="flex flex-row justify-center text-5xl m-4">I'm Jorge Villalobos Frontend Developer</h1>
+<h1 class="flex justify-center text-5xl m-4">I'm Jorge Villalobos Frontend Developer</h1>
 </div>
 
-<div id="aboutMe" class="bg-gray-800 rounded-lg p-4 mt-14 text-center">
-    <h2 class="text-2xl">About Me</h2>
-    <p class="mt-2">I am a passionate frontend developer with a love for creating beautiful and functional web applications.</p>
+<div id="aboutMe" class="bg-gray-500 rounded-lg p-4 mt-14 text-center">
+    <h2 class="text-3xl">About Me</h2>
+    <p class="mt-2">I am a passionate frontend developer with a love for creating useful, creative, modern, and functional web applications.
+        As a Computer Engineering student I'm working with different technologies and tools, check out my projects and enjoy the trip.</p>
 </div>
 
 </template>
--- src/main.js
+++ src/main.js
@@ -3,14 +3,37 @@ import './style.css' // Import global styles
 import App from './App.vue' // Import the main App component
 import PrimeVue from 'primevue/config' // Import PrimeVue configuration
 import Aura from '@primeuix/themes/aura' // Import the Aura theme from PrimeUIX
-
+import { definePreset } from '@primeuix/themes'
 
 createApp(App).mount('#app') // Initialize the Vue application and mount it to the DOM
+
 const app = createApp(App); // Create a Vue application instance
-app.use(PrimeVue, {
-    theme: {
-        preset: Aura
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{amber.50}',
+            100: '{amber.100}',
+            200: '{amber.200}',
+            300: '{amber.300}',
+            400: '{amber.400}',
+            500: '{amber.500}',
+            600: '{amber.600}',
+            700: '{amber.700}',
+            800: '{amber.800}',
+            900: '{amber.900}',
+            950: '{amber.950}'
+        }
     }
 });
 
-app.mount('#app') // Mount the application to the DOM
+
+
+app.use(PrimeVue, {
+    theme: {
+        preset: MyPreset,
+        options: {
+            darkModeSelector: '.my-app-dark',
+        }
+    }
+});
