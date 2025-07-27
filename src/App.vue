d2a1fba0a59d705a1d94386ec1cbcb62d0df8bd9
Jorge_Villa <jorgevihe@gmail.com>
2025-07-27 10:13:58 -0600

    Accordion for projects added, project's titles and descriptions included.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <script setup> 
 
-import { ref } from 'vue'; // Importing Vue's ref for reactivity
+
 import Button from 'primevue/button'; // Importing PrimeVue Button component
+import Accordion from 'primevue/accordion'; // Importing PrimeVue Accordion component
+import AccordionPanel from 'primevue/accordionpanel'; // Importing PrimeVue Accordion components
+import AccordionHeader from 'primevue/accordionheader'; // Importing PrimeVue Accordion components
+import AccordionContent from 'primevue/accordioncontent'; // Importing PrimeVue Accordion components
+import Card from 'primevue/card'; // Importing PrimeVue Card component
+
 
 function toggleDarkMode() {
     document.documentElement.classList.toggle('my-app-dark');
 }
-
 </script>
+
+
 
 <template>
 
-<Button label="Theme" @click="toggleDarkMode()" />
-
+<!-- Button to toggle dark/light mode -->
 <div>
-<h1 class="flex justify-center text-5xl m-4">I'm Jorge Villalobos Frontend Developer</h1>
+    <Button style="background-color: lightslategray; padding: 0.4rem; border-radius: 10px;" label="Mode" @click="toggleDarkMode()" />
 </div>
 
-<div id="aboutMe" class="bg-gray-500 rounded-lg p-4 mt-14 text-center">
-    <h2 class="text-3xl">About Me</h2>
-    <p class="mt-2">I am a passionate frontend developer with a love for creating useful, creative, modern, and functional web applications.
-        As a Computer Engineering student I'm working with different technologies and tools, check out my projects and enjoy the trip.</p>
+<div>
+<h1 class="flex justify-center items-center text-5xl m-4">I'm Jorge Villalobos Frontend Developer</h1>
+</div>
+
+<!-- Card component to display about me section -->
+<div class="flex justify-center items-center m-20">
+    <Card>
+        <template #title>About me</template>
+        <template #content>
+            <p class="my-3">
+                I am a passionate Computer Engineer student and Frontend Developer with a focus on creating useful and functional user interfaces and applications. 
+                I have experience in Vue.js, JavaScript, and CSS, and I love to build applications that provide a great user experience.
+                My goal is to continuously learn and improve my skills while contributing to exciting projects.
+            </p>
+        </template>
+    </Card>
+</div>
+
+<!-- Accordion component to display projects -->
+<div>
+    <Accordion :value="['0']" multiple>
+        <AccordionPanel value="0">
+            <AccordionHeader> <a href="">AI agentic / Chatbot</a> </AccordionHeader>
+            <AccordionContent>
+                <p class="pt-1">
+                    This project was developed using Gemini, Tailwind, Vue.js and PrimeVue. 
+                    It features a chatbot that can answer questions about various topics, including programming, technology, and general knowledge. 
+                    The chatbot uses an AI model to generate responses based on user input.
+                </p>
+            </AccordionContent> 
+        </AccordionPanel>
+        <AccordionPanel value="1">
+            <AccordionHeader> <a href="">E-commerece website (Frontend)</a> </AccordionHeader>
+            <AccordionContent>
+                <p class="pt-1">
+                    This project is a frontend e-commerce website developed using Tailwind, Vue.js and PrimeVue. 
+                    It features a user-friendly interface for browsing products, adding items to the cart, and checking out. 
+                    The website is designed to provide a seamless shopping experience for users.
+                </p>
+            </AccordionContent>
+        </AccordionPanel>
+        <AccordionPanel value="2">
+            <AccordionHeader> <a href="">Semi-automatic AI Pomodoro</a> </AccordionHeader>
+            <AccordionContent>
+                <p class="pt-1">
+                    This project is a semi-automatic Pomodoro timer developed using Vue.js and PrimeVue. 
+                    It allows users to set work and break intervals, track their productivity, and receive notifications when it's time to take a break or resume work. 
+                    AI integration helps users suggesting time periods baseed on their tasks and preferences.
+                    The application is designed to help users manage their time effectively and improve focus.
+                </p>
+            </AccordionContent>
+        </AccordionPanel>
+    </Accordion>
 </div>
 
 </template>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import { createApp } from 'vue' // Create a Vue application
-import './style.css' // Import global styles
-import App from './App.vue' // Import the main App component
-import PrimeVue from 'primevue/config' // Import PrimeVue configuration
-import Aura from '@primeuix/themes/aura' // Import the Aura theme from PrimeUIX
-import { definePreset } from '@primeuix/themes'
+import { createApp } from 'vue'; // Create a Vue application
+import './style.css'; // Import global styles
+import App from './App.vue'; // Import the main App component
+import PrimeVue from 'primevue/config'; // Import PrimeVue configuration
+import Aura from '@primeuix/themes/aura'; // Import the Aura theme from PrimeUIX
+import { definePreset } from '@primeuix/themes'; // Import the definePreset function to create a custom theme preset
+
 
 createApp(App).mount('#app') // Initialize the Vue application and mount it to the DOM
 
@@ -11,22 +12,42 @@ const app = createApp(App); // Create a Vue application instance
 
 const MyPreset = definePreset(Aura, {
     semantic: {
-        primary: {
-            50: '{amber.50}',
-            100: '{amber.100}',
-            200: '{amber.200}',
-            300: '{amber.300}',
-            400: '{amber.400}',
-            500: '{amber.500}',
-            600: '{amber.600}',
-            700: '{amber.700}',
-            800: '{amber.800}',
-            900: '{amber.900}',
-            950: '{amber.950}'
+        colorScheme: {
+            light: {
+                surface: {
+                    0: 'black',
+                    50: '{teal.50}',
+                    100: '{teal.100}',
+                    200: '{teal.200}',
+                    300: '{teal.300}',
+                    400: '{teal.400}',
+                    500: '{teal.500}',
+                    600: '{teal.600}',
+                    700: '{teal.700}',
+                    800: '{teal.800}',
+                    900: '{teal.900}',
+                    950: '{teal.950}'
+                }
+            },
+            dark: {
+                surface: {
+                    0: 'white',
+                    50: '{amber.50}',
+                    100: '{amber.100}',
+                    200: '{amber.200}',
+                    300: '{amber.300}',
+                    400: '{amber.400}',
+                    500: '{amber.500}',
+                    600: '{amber.600}',
+                    700: '{amber.700}',
+                    800: '{amber.800}',
+                    900: '{amber.900}',
+                    950: '{amber.950}'
+                }
+            } 
         }
     }
 });
-
 
 
 app.use(PrimeVue, {
