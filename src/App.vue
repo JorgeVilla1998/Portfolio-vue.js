779b1d6adec3c15aa659beea4e95d41224214e2e
Jorge_Villa <jorgevihe@gmail.com>
2025-07-27 19:47:56 -0600

    Bugs fixed, color changed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup> 
 
-
 import Button from 'primevue/button'; // Importing PrimeVue Button component
 import Accordion from 'primevue/accordion'; // Importing PrimeVue Accordion component
 import AccordionPanel from 'primevue/accordionpanel'; // Importing PrimeVue Accordion components
@@ -12,19 +11,18 @@ import Card from 'primevue/card'; // Importing PrimeVue Card component
 function toggleDarkMode() {
     document.documentElement.classList.toggle('my-app-dark');
 }
+
 </script>
-
-
 
 <template>
 
 <!-- Button to toggle dark/light mode -->
-<div>
-    <Button style="background-color: lightslategray; padding: 0.4rem; border-radius: 10px;" label="Mode" @click="toggleDarkMode()" />
-</div>
+
+<Button label="Theme" @click="toggleDarkMode()" />
+
 
 <div>
-<h1 class="flex justify-center items-center text-5xl m-4">I'm Jorge Villalobos Frontend Developer</h1>
+    <h1 class="flex justify-center items-center text-5xl m-4">I'm Jorge Villalobos Frontend Developer</h1>
 </div>
 
 <!-- Card component to display about me section -->
@@ -42,41 +40,41 @@ function toggleDarkMode() {
 </div>
 
 <!-- Accordion component to display projects -->
-<div>
-    <Accordion :value="['0']" multiple>
-        <AccordionPanel value="0">
-            <AccordionHeader> <a href="">AI agentic / Chatbot</a> </AccordionHeader>
-            <AccordionContent>
-                <p class="pt-1">
-                    This project was developed using Gemini, Tailwind, Vue.js and PrimeVue. 
-                    It features a chatbot that can answer questions about various topics, including programming, technology, and general knowledge. 
-                    The chatbot uses an AI model to generate responses based on user input.
-                </p>
-            </AccordionContent> 
-        </AccordionPanel>
-        <AccordionPanel value="1">
-            <AccordionHeader> <a href="">E-commerece website (Frontend)</a> </AccordionHeader>
-            <AccordionContent>
-                <p class="pt-1">
-                    This project is a frontend e-commerce website developed using Tailwind, Vue.js and PrimeVue. 
-                    It features a user-friendly interface for browsing products, adding items to the cart, and checking out. 
-                    The website is designed to provide a seamless shopping experience for users.
-                </p>
-            </AccordionContent>
-        </AccordionPanel>
-        <AccordionPanel value="2">
-            <AccordionHeader> <a href="">Semi-automatic AI Pomodoro</a> </AccordionHeader>
-            <AccordionContent>
-                <p class="pt-1">
-                    This project is a semi-automatic Pomodoro timer developed using Vue.js and PrimeVue. 
-                    It allows users to set work and break intervals, track their productivity, and receive notifications when it's time to take a break or resume work. 
-                    AI integration helps users suggesting time periods baseed on their tasks and preferences.
-                    The application is designed to help users manage their time effectively and improve focus.
-                </p>
-            </AccordionContent>
-        </AccordionPanel>
-    </Accordion>
-</div>
+
+<Accordion :value="['0']" multiple>
+    <AccordionPanel value="0">
+        <AccordionHeader> <a href="">AI agentic / Chatbot</a> </AccordionHeader>
+        <AccordionContent>
+            <p class="pt-1">
+                This project was developed using Gemini, Tailwind, Vue.js and PrimeVue. 
+                It features a chatbot that can answer questions about various topics, including programming, technology, and general knowledge. 
+                The chatbot uses an AI model to generate responses based on user input.
+            </p>
+        </AccordionContent> 
+    </AccordionPanel>
+    <AccordionPanel value="1">
+        <AccordionHeader> <a href="">E-commerece website (Frontend)</a> </AccordionHeader>
+        <AccordionContent>
+            <p class="pt-1">
+                This project is a frontend e-commerce website developed using Tailwind, Vue.js and PrimeVue. 
+                It features a user-friendly interface for browsing products, adding items to the cart, and checking out. 
+                The website is designed to provide a seamless shopping experience for users.
+            </p>
+        </AccordionContent>
+    </AccordionPanel>
+    <AccordionPanel value="2">
+        <AccordionHeader> <a href="">Semi-automatic AI Pomodoro</a> </AccordionHeader>
+        <AccordionContent>
+            <p class="pt-1">
+                This project is a semi-automatic Pomodoro timer developed using Vue.js and PrimeVue. 
+                It allows users to set work and break intervals, track their productivity, and receive notifications when it's time to take a break or resume work. 
+                AI integration helps users suggesting time periods baseed on their tasks and preferences.
+                The application is designed to help users manage their time effectively and improve focus.
+            </p>
+        </AccordionContent>
+    </AccordionPanel>
+</Accordion>
+
 
 </template>
 
--- src/main.js
+++ src/main.js
@@ -5,48 +5,32 @@ import PrimeVue from 'primevue/config'; // Import PrimeVue configuration
 import Aura from '@primeuix/themes/aura'; // Import the Aura theme from PrimeUIX
 import { definePreset } from '@primeuix/themes'; // Import the definePreset function to create a custom theme preset
 
-
-createApp(App).mount('#app') // Initialize the Vue application and mount it to the DOM
-
 const app = createApp(App); // Create a Vue application instance
 
 const MyPreset = definePreset(Aura, {
     semantic: {
-        colorScheme: {
-            light: {
-                surface: {
-                    0: 'black',
-                    50: '{teal.50}',
-                    100: '{teal.100}',
-                    200: '{teal.200}',
-                    300: '{teal.300}',
-                    400: '{teal.400}',
-                    500: '{teal.500}',
-                    600: '{teal.600}',
-                    700: '{teal.700}',
-                    800: '{teal.800}',
-                    900: '{teal.900}',
-                    950: '{teal.950}'
-                }
-            },
-            dark: {
-                surface: {
-                    0: 'white',
-                    50: '{amber.50}',
-                    100: '{amber.100}',
-                    200: '{amber.200}',
-                    300: '{amber.300}',
-                    400: '{amber.400}',
-                    500: '{amber.500}',
-                    600: '{amber.600}',
-                    700: '{amber.700}',
-                    800: '{amber.800}',
-                    900: '{amber.900}',
-                    950: '{amber.950}'
-                }
-            } 
+        primary: {
+            50: '{slate.50}',
+            100: '{slate.100}',
+            200: '{slate.200}',
+            300: '{slate.300}',
+            400: '{slate.400}',
+            500: '{slate.500}',
+            600: '{slate.600}',
+            700: '{slate.700}',
+            800: '{slate.800}',
+            900: '{slate.900}',
+            950: '{slate.950}'
+        },  
+    },
+    components: {    
+        Button: {
+            paddingY: '4rem',
+        },
+        Card: {
+            background: 'var(--red-500)', // Set the background color for Card component
         }
-    }
+    }          
 });
 
 
@@ -58,3 +42,5 @@ app.use(PrimeVue, {
         }
     }
 });
+
+createApp(App).mount('#app') // Initialize the Vue application and mount it to the DOM
